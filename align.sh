d7325f28008138e4bc613ae9dda35a65da2e1dd3
passet <tom.passey@emr.ac.uk>
2014-03-13 11:43:05 +0000

    Added some documentation

--- align.sh
+++ align.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# Assemble contigs using velvet and generate summary statistics using 
+# process_contigs.pl
+#
 velveth P.cact_assembly 45 -fastq -shortPaired -separate P.cact_411_1M_F_trim.fastq P.cact_411_1M_R_trim.fastq
 velvetg P.cact_assembly -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
 process_contigs.pl -i P.cact_assembly/contigs.fa -o process_contigs_P.cact.1
