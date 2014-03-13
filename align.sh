0026f226b859d8e2aea2c73fdb9b5b152536b65e
passet <tom.passey@emr.ac.uk>
2014-03-13 12:28:55 +0000

    Added variables

--- align.sh
+++ align.sh
@@ -1,9 +1,17 @@
-#!/bin/bash
+#!/bin/bash 
 # Assemble contigs using velvet and generate summary statistics using 
 # process_contigs.pl
 #
-velveth P.cact_assembly 45 -fastq -shortPaired -separate P.cact_411_1M_F_trim.fastq P.cact_411_1M_R_trim.fastq
-velvetg P.cact_assembly -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
-process_contigs.pl -i P.cact_assembly/contigs.fa -o process_contigs_P.cact.1
+WORK_DIR=P.cact_assembly
+HASH_LENGTH=$1
+FORWARD_FILE=P.cact_411_1M_F_trim.fastq
+REVERSE_FILE=P.cact_411_1M_R_trim.fastq
+ASSEMBLY_NAME=P.cact
 
+echo "hash length is $HASH_LENGTH"
 
+velveth $WORK_DIR $HASH_LENGTH -fastq -shortPaired -separate $FORWARD_FILE $REVERSE_FILE
+velvetg $WORK_DIR -long_mult_cutoff 1 -exp_cov 6 -ins_length 700 -cov_cutoff 2 -min_contig_lgth 750
+process_contigs.pl -i $WORK_DIR/contigs.fa -o $ASSEMLY_NAME.$HASH_LENGTH
+
+rm -R $WORK_DIR
